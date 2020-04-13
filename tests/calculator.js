2d4b08545f77c1167e28c2e648b7afe5b86f1e72
Aquino Melo <862343@cognizant.com>
2020-04-13 14:16:36 -0500

    my first commit project

---
+++ tests/calculator.js
@@ -0,0 +1,42 @@
+let homepage = require('../pages/homepage')
+
+describe('Demo calculator test', function(){
+
+    it('Addition test', function(){
+
+        //browser.get('http://juliemr.github.io/protractor-demo/');
+        homepage.get('http://juliemr.github.io/protractor-demo/');
+
+        //element(by.model('first')).sendKeys('2');
+        homepage.enterFirstNumber('4');
+
+        //element(by.model('second')).sendKeys('3');
+        homepage.enterSecondNumber('3');
+
+        //element(by.css('[ng-click="doAddition()"]')).click();
+        homepage.clickGo();
+
+        //let result = element(by.cssContainingText('.ng-binding', '5'));
+        //expect(result.getText()).toEqual('5');
+        homepage.verifyResult('7');
+
+        browser.sleep(3000);
+
+    });
+
+    it('subtraction test', function () {
+
+        homepage.get('http://juliemr.github.io/protractor-demo/');
+
+        homepage.enterFirstNumber('4');
+
+        homepage.enterSecondNumber('3');
+
+        homepage.clickGo1();
+
+        homepage.verifyResult('7');
+
+        browser.sleep(2000)
+
+    });
+});
